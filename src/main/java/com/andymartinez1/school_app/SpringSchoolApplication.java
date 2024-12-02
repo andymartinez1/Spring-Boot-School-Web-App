7ba98aaa5dfb5392677dc7c9ea6a970266d9c5d0
Andrew Martinez <andy72391@gmail.com>
2024-12-02 06:53:29 -0500

    Starting a school web app project using Spring Boot for the backend and Thymeleaf for the frontend.

---
+++ src/main/java/com/andymartinez1/school_app/SpringSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.andymartinez1.school_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSchoolApplication.class, args);
+    }
+
+}
